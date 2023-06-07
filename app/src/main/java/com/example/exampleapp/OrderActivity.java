177d2c9d3e0b70843cf3b6a6adc588577b827a62
2005934 <oh2976@gmail.com>
2023-06-07 15:37:22 +0900

    장바구니 데이터 주문데이터로 읽기 수정 완료(일부 오류 존재) (* delete 오류, totalPrice 오류 *)

--- app/src/main/java/com/example/exampleapp/OrderActivity.java
+++ app/src/main/java/com/example/exampleapp/OrderActivity.java
@@ -3,20 +3,29 @@ package com.example.exampleapp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -28,17 +37,56 @@ public class OrderActivity extends AppCompatActivity {
     FirebaseAuth firebaseAuth;
     DatabaseReference databaseReference;
 
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter adapter;
+    private RecyclerView.LayoutManager layoutManager;
+    private ArrayList<Cart> arrayList;
+
+    TextView name, price;
+
+    Button btnPayment;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order);
 
+        recyclerView = findViewById(R.id.recyclerView_order); //아디 연결
+        recyclerView.setHasFixedSize(true); //리사이클러뷰 기존 성능 강화
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        arrayList = new ArrayList<>(); // Product 객체를 담을 어레이리스트(어댑터 쪽으로 날릴 거임)
+
         firebaseAuth = FirebaseAuth.getInstance();
         firebaseDatabase = FirebaseDatabase.getInstance();
-
-
+        FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
 
         databaseReference = FirebaseDatabase.getInstance().getReference("CurrentUser");
+
+        databaseReference.child(firebaseUser.getUid()).child("AddToCart").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                // 파이어베이스 데이터베이스의 데이터를 받아오는 곳
+                arrayList.clear(); //기존 배열 리스트가 존재하지 않게 남아 있는 데이터 초기화
+                for(DataSnapshot snapshot : dataSnapshot.getChildren()){
+                    // 반복문으로 데이터 List를 추출해냄
+                    Cart cart = snapshot.getValue(Cart.class); //  만들어 뒀던 Product 객체에 데이터를 담는다.
+                    arrayList.add(cart); // 담은 데이터들을 배열 리스트에 넣고 리사이클러뷰로 보낼 준비
+
+                }
+                adapter.notifyDataSetChanged(); // 리스트 저장 및 새로고침
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                // 디비를 가져오던 중 에러 발생 시
+                Log.e("OrderActivity", String.valueOf(databaseError.toException())); // 에러문 출력
+            }
+        });
+
+        adapter = new CartAdapter(this, arrayList);
+        recyclerView.setAdapter(adapter); //리사이클러뷰에 어댑터 연결
 
         String orderId = databaseReference.push().getKey();
 
@@ -50,46 +98,39 @@ public class OrderActivity extends AppCompatActivity {
 
         List<Cart> list = (ArrayList<Cart>) getIntent().getSerializableExtra("itemList");
 
-        FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
 
 
-
-        if (list != null && list.size() > 0) {
-            for (Cart model : list) {
+        btnPayment = (Button) findViewById(R.id.btnPayment);
 
 
-                databaseReference.child(firebaseUser.getUid()).child("MyOrder").child(orderId).setValue(cartMap).addOnCompleteListener(new OnCompleteListener<Void>() {
-                    @Override
-                    public void onComplete(@NonNull Task<Void> task) {
-                        Toast.makeText(OrderActivity.this, "주문완료", Toast.LENGTH_SHORT).show();
+        btnPayment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (list != null && list.size() > 0) {
+                    for (Cart model : list) {
+
+                        final HashMap<String, Object> cartMap = new HashMap<>();
+
+                        cartMap.put("productName", model.getProductName());
+                        cartMap.put("productPrice", model.getProductPrice());
+                        cartMap.put("totalQuantity", model.getTotalQuantity());
+                        cartMap.put("totalPrice", model.getTotalPrice());
+
+                        databaseReference.child(firebaseUser.getUid()).child("MyOrder").child(orderId).setValue(cartMap).addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                Toast.makeText(OrderActivity.this, "주문완료", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+
                     }
-                });
+
+
+                }
 
             }
+        });
 
-
-        }
-
-        if (list != null && list.size() > 0) {
-            for (Cart model : list) {
-
-                final HashMap<String, Object> cartMap = new HashMap<>();
-
-                cartMap.put("productName", model.getProductName());
-                cartMap.put("productPrice", model.getProductPrice());
-                cartMap.put("totalQuantity", model.getTotalQuantity());
-                cartMap.put("totalPrice", model.getTotalPrice());
-
-                databaseReference.child(firebaseUser.getUid()).child("MyOrder").child(orderId).setValue(cartMap).addOnCompleteListener(new OnCompleteListener<Void>() {
-                    @Override
-                    public void onComplete(@NonNull Task<Void> task) {
-                        Toast.makeText(OrderActivity.this, "주문완료", Toast.LENGTH_SHORT).show();
-                    }
-                });
-
-            }
-
-
-        }
     }
 }
